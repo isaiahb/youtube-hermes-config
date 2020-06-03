8b99d150546ef6b016b417d4d57b887facb81548
Isaiah Ballah <ballah@google.com>
2020-06-03 16:43:48 +0000

    sucesfully receive and acknowledge messages from subscribed topic

--- cpp_subscriber/main.cc
+++ cpp_subscriber/main.cc
@@ -15,59 +15,28 @@ int main() {
 	auto channel = grpc::CreateChannel("pubsub.googleapis.com", credentials);
 	std::unique_ptr<Subscriber::Stub> stub(Subscriber::NewStub(channel));
 
-	// // Open up the stream.
+	// Open stream.
     ClientContext context;
     auto stream(stub->StreamingPull(&context));
 
-    // // Send initial message.
+    // connect stream to pubsub subscription
     StreamingPullRequest request;
-    request.set_subscription("projects/lab5-260516/subscriptions/testsub");
+    request.set_subscription("projects/google.com:youtube-admin-pacing-server/subscriptions/CppBinary");
     request.set_stream_ack_deadline_seconds(10);
     stream->Write(request);
 
-    // Receive messages.
+    // poll for messages.
     StreamingPullResponse response;
     while (stream->Read(&response)) {
-      // Ack messages.
+      // acknowledged messages.
     	StreamingPullRequest ack_request;
     	for (const auto &message : response.received_messages()) {
     		ack_request.add_ack_ids(message.ack_id());
-			std::cout<<"message"<<std::endl;
+			std::cout<<"message received and acknowledged"<<std::endl;
+           //std::cout<<message<<std::endl; //figure out how to properly get message text
     	}
     	stream->Write(ack_request);
     }
 
-	std::cout<<"wow"<<std::endl;
+	std::cout<<"program finished"<<std::endl;
 }
-
-
-
-// //publish  example
-// auto main() -> int {
-//     using grpc::ClientContext;
-//     using google::pubsub::v1::Publisher;
-//     using google::pubsub::v1::PublishRequest;
-//     using google::pubsub::v1::PublishResponse;
-//     using google::pubsub::v1::PubsubMessage;
-
-//     auto creds = grpc::GoogleDefaultCredentials();
-// 	auto channel = grpc::CreateChannel("pubsub.googleapis.com", creds);
-// 	std::unique_ptr<Publisher::Stub> stub(Publisher::NewStub(channel));
-
-//     PublishRequest request;
-//     auto const topic_string = "projects/lab5-260516/topics/testing";
-//     request.set_topic(topic_string);
-//     PubsubMessage msg;
-//     msg.set_data("Hello from C++");
-//     *request.add_messages() = msg;
-
-//     PublishResponse response;
-//     ClientContext ctx;
-
-//     auto status = stub->Publish(&ctx, request, &response);
-//     if (!status.ok()) {
-//         std::cout << "Publishing message failed with error " + std::to_string(status.error_code()) + ": " + status.error_message() << '\n';
-//     }
-// }
-
-//gcloud auth application-default login //<- cmd can be used to set GoogleDefaultCredentials

67cde9faf83f41037491b655b709f09f22270505
Isaiah Ballah <ballah@google.com>
2020-06-04 19:31:57 +0000

    fixed keeping program alive by creating new context when stream closes

--- cpp_subscriber/main.cc
+++ cpp_subscriber/main.cc
@@ -19,25 +19,25 @@ int main() {
 	auto credentials = grpc::GoogleDefaultCredentials();	
 	auto channel = grpc::CreateChannel("pubsub.googleapis.com", credentials);
 	std::unique_ptr<Subscriber::Stub> stub(Subscriber::NewStub(channel));
-
-	// Open stream.
-	ClientContext context;
-	auto stream(stub->StreamingPull(&context));
-	// connect stream to pubsub subscription
-	StreamingPullRequest request;
-	request.set_subscription("projects/google.com:youtube-admin-pacing-server/subscriptions/CppBinary");
-	request.set_stream_ack_deadline_seconds(10);
-	stream->Write(request);
-
-	// poll for messages.
-	int i = 0;
-	StreamingPullResponse response;
-
+	
 	//keep alive for duration of machines life
 	while (true) {
+		std::this_thread::sleep_for(std::chrono::seconds(10));
+
+		// Open stream.
+		ClientContext context;
+		auto stream(stub->StreamingPull(&context));
+		// connect stream to pubsub subscription
+		StreamingPullRequest request;
+		request.set_subscription("projects/google.com:youtube-admin-pacing-server/subscriptions/CppBinary");
+		request.set_stream_ack_deadline_seconds(10);
+
+		// poll for messages.
+		StreamingPullResponse response;
+		stream->Write(request);
+
 		while (stream->Read(&response)) {
-			i++;
-			std::cout<<"Reading: "<<i<<std::endl;
+			std::cout<<"Reading: "<<std::endl;
 			StreamingPullRequest ack_request;
 			for (const auto &message : response.received_messages()) {
 				ack_request.add_ack_ids(message.ack_id());
@@ -50,11 +50,9 @@ int main() {
 			// acknowledged messages.
 			stream->Write(ack_request);
 		}
-		std::this_thread::sleep_for(std::chrono::seconds(1));
 	}
-	std::cout<<"program finished"<<std::endl;
+	std::cout<<"program ending"<<std::endl;
 }
-
 
 void Process(std::string message) {
 	std::cout<<"message received: "<<message<<std::endl;
